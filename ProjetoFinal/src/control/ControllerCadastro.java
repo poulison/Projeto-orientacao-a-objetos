df54382827f907eef685ac52e6646ab19713254e
Paulo Hirata <127874722+poulison@users.noreply.github.com>
2024-05-07 22:58:01 -0300

    Add files via upload

---
+++ ProjetoFinal/src/control/ControllerCadastro.java
@@ -0,0 +1,36 @@
+
+package control;
+
+import DAO.InvestidorDAO;
+import DAO.Conexao;
+import model.Investidor;
+import view.Cadastro;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+public class ControllerCadastro {
+    private Cadastro view;
+
+    public ControllerCadastro(Cadastro view) {
+        this.view = view;
+    }
+    
+       public void salvarAluno(){  
+        String nome = view.getTxtnome().getText();
+        String CPF = view.getTxtCPF().getText();
+        String senha = view.getTxtsenha().getText();
+        
+        Investidor investidor = new Investidor(nome, CPF, senha);
+        Conexao conexao = new Conexao();
+        
+        try{
+            Connection conn = conexao.getConnection();
+            InvestidorDAO dao = new InvestidorDAO(conn);
+            dao.inserir(investidor);
+            JOptionPane.showMessageDialog(view, "Usuario Cadastrado!");
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(view, "Usuario nao Cadastrado!");
+        }
+    }
+}
